5d663f20d433819533a8d8929ad8916353862b73
Paul Pineda <paul@pineda.codes>
2017-06-25 01:55:42 +0100

    Added mulitple invocation and average of array of tiems methods.  HAve not yet exposed them as they can be made more generic

--- perf.js
+++ perf.js
@@ -11,6 +11,27 @@
     return t1-t0;
   };
 
+  function invokeMultipleTimes(n,fn){
+    var results = [],
+      args = [].slice.call(arguments, 1),
+      timesToInvoke = args[args.length-1];
+
+    for(var i=0; i<n; i++){
+      results.push(timeToExecute.apply(null, args));
+    }
+    return results;
+  };
+
+  function averageTimeToComplete(array){
+    return array.reduce(
+      function(acc,c)
+      {
+        return (acc += c);
+      },
+      0
+    )/array.length * 100;
+  }
+
   exports.perf = {
     timeFunction: timeFunction
   }
@@ -19,3 +40,4 @@
   ? exports
   : window
 );
+
